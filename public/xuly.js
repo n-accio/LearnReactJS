64b70333afa6fb337524c103d219cd4b35baa165
n-accio <truongthihongnhi1998@gmail.com>
2020-03-20 07:53:41 +0700

    Push bai 6

---
+++ public/xuly.js
@@ -0,0 +1,10 @@
+class KhoaPham extends React.Component{
+  render() {
+    return <h1 className="violet"> Khoa Pham React</h1>
+  }
+};
+
+ReactDOM.render( 
+    <KhoaPham /> 
+  , document.getElementById("root")
+);
