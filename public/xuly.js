11918e1367c297c7318c0dba86c704352b54f6cf
n-accio <truongthihongnhi1998@gmail.com>
2020-03-24 16:25:42 +0700

    Push Bai_12

--- public/xuly.js
+++ public/xuly.js
@@ -3,9 +3,15 @@ function getName(name){
 }
 
 class KhoaPham extends React.Component{
+
+  addStudent(){
+    this.setState({tongHocVien: this.state.tongHocVien + 1});
+  }
   constructor(props) {
     super(props);
     this.layThongTin = this.layThongTin.bind(this);
+    this.state = {tongHocVien: 0};
+    this.addStudent= this.addStudent.bind(this);
   }
   layThongTin() {
     alert(this.props.children);
@@ -15,10 +21,12 @@ class KhoaPham extends React.Component{
     return (
       <div>
         <h1 className="violet"> {this.props.subject} - {this.props.lecturers}</h1>
+        <div>So hoc vien: {this.state.tongHocVien}</div>
         <p> {this.props.children} </p>
         <button onClick={
           () =>{var str= this.props.lecturers + " "+ this.props.subject; getName(str)}
         }>Thong tin</button>
+        <button onClick={this.addStudent}>Đang Ky Hoc</button>
         <h3>Lap Trinh ReatJs</h3>
       </div>
     )
