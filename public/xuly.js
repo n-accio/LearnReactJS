301ed6478c76f333eb6451e645cc83cd7a5043ae
n-accio <truongthihongnhi1998@gmail.com>
2020-03-26 08:57:36 +0700

    Push Bai_14

--- public/xuly.js
+++ public/xuly.js
@@ -3,24 +3,24 @@ function getName(name){
 }
 
 class KhoaPham extends React.Component{
-
+  
   constructor(props) {
     super(props);
     this.layThongTin = this.layThongTin.bind(this);
-    this.state = {tongHocVien: this.props.tongHocVien};
+     this.state = {tongHocVien: this.props.tongHocVien};
     this.addStudent= this.addStudent.bind(this);
   }
+  
   addStudent(){
     this.setState({tongHocVien: parseInt(this.state.tongHocVien) + 1});
     // this.state.tongHocVien = parseInt(this.state.tongHocVien)+1;
-    // this.setState(this.state);
+    // this.setState(this.state); 
   }
   layThongTin() {
     alert(this.props.children);
   }
-
   render() {
-    return (
+    return(
       <div>
         <h1 className="violet"> {this.props.subject} - {this.props.lecturers}</h1>
         <div>So hoc vien: {this.state.tongHocVien}</div>
@@ -35,10 +35,32 @@ class KhoaPham extends React.Component{
   }
 };
 
+class InputTag extends React.Component{
+  show(){
+    var text = this.refs.sl.value;
+    //var text = this.refs.txt.value;
+    alert(text);
+  }
+  render(){
+    return (
+      <div>
+      <select ref="sl">
+        <option value="o">Oppo</option>
+        <option value="ss">Samsung </option>
+        <option value="ip">Iphone </option>
+      </select>
+      <input type="text" ref="txt"/>
+      <button onClick={this.show.bind(this)}>Hien thi</button>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render( 
   <div>
+    <InputTag />
     <KhoaPham subject="ReactJs" lecturers="Mr Khoa Pham" tongHocVien="10"> Mon hoc React</KhoaPham>
-      <KhoaPham subject="Node Js" lecturers="Mr Thinh" tongHocVien="20">Mon hoc Node</KhoaPham> 
+    <KhoaPham subject="Node Js" lecturers="Mr Thinh" tongHocVien="20">Mon hoc Node</KhoaPham> 
   </div>
   , document.getElementById("root")
 );
